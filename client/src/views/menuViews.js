997d75c81d245e823f65d15bb2e07f7ba436bbb2
xHozey <hamzasaliheddine@hotmail.com>
2025-04-30 16:13:33 +0100

    change: dom creation into string

--- client/src/views/menuViews.js
+++ client/src/views/menuViews.js
@@ -1,33 +1,41 @@
 class Menu {
-  constructor(ws) {
-    this.ws = ws;
-    this.nickname = "";
+    constructor(ws) {
+      this.ws = ws;
+      this.nickname = "";
+    }
+  
+    render() {
+      const template = `
+        <div>
+          <h1>BOMBER MAN</h1>
+          <input id="nicknameInput" type="text">
+          <button id="playButton">Play</button>
+        </div>
+      `;
+      
+      const div = document.createElement('div');
+      div.innerHTML = template;
+      
+      const input = div.querySelector('#nicknameInput');
+      const btn = div.querySelector('#playButton');
+      
+      input.addEventListener('input', (e) => {
+        this.nickname = e.target.value;
+      });
+      
+      btn.onclick = () => {
+        this.handleSubmition();
+      };
+      
+      return div;
+    }
+  
+    handleSubmition() {
+      this.ws.send({
+        type: "authentification",
+        nickname: this.nickname,
+      });
+    }
   }
-
-  render() {
-    const div = document.createElement("div");
-    div.innerHTML += `<h1>BOMBER MAN</h1>`;
-    const input = document.createElement("input");
-    input.addEventListener("input", (e) => {
-      this.nickname = e.target.value;
-    });
-    div.appendChild(input);
-    const btn = document.createElement("button");
-    btn.innerText = "Play";
-    div.appendChild(btn);
-
-    btn.onclick = () => {
-      this.handleSubmition();
-    };
-    return div;
-  }
-
-  handleSubmition() {
-    this.ws.send({
-      type: "authentification",
-      nickname: this.nickname,
-    });
-  }
-}
-
-export default Menu;
+  
+  export default Menu;
