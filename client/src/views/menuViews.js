3e84bacc6840607b3063b14c315421cd383c2b1d
Aymen Hiti <aymenhiti24@gmail.com>
2025-04-30 18:05:57 +0100

    added gameviews and lobby views styles

--- client/src/views/menuViews.js
+++ client/src/views/menuViews.js
@@ -1,41 +1,41 @@
 class Menu {
-    constructor(ws) {
-      this.ws = ws;
-      this.nickname = "";
-    }
-  
-    render() {
-      const template = `
+  constructor(ws) {
+    this.ws = ws;
+    this.nickname = "";
+  }
+
+  render() {
+    const template = `
         <div>
-          <h1>BOMBER MAN</h1>
-          <input id="nicknameInput" type="text">
-          <button id="playButton">Play</button>
+          <h1 class="title">BOMBER MAN</h1>
+          <input class="nickname" id="nicknameInput" type="text" placeholder="enter your name!">
+          <button class="play" id="playButton">Play</button>
         </div>
       `;
-      
-      const div = document.createElement('div');
-      div.innerHTML = template;
-      
-      const input = div.querySelector('#nicknameInput');
-      const btn = div.querySelector('#playButton');
-      
-      input.addEventListener('input', (e) => {
-        this.nickname = e.target.value;
-      });
-      
-      btn.onclick = () => {
-        this.handleSubmition();
-      };
-      
-      return div;
-    }
-  
-    handleSubmition() {
-      this.ws.send({
-        type: "authentification",
-        nickname: this.nickname,
-      });
-    }
+
+    const div = document.createElement('div');
+    div.innerHTML = template;
+
+    const input = div.querySelector('#nicknameInput');
+    const btn = div.querySelector('#playButton');
+
+    input.addEventListener('input', (e) => {
+      this.nickname = e.target.value;
+    });
+
+    btn.onclick = () => {
+      this.handleSubmition();
+    };
+
+    return div;
   }
-  
-  export default Menu;
+
+  handleSubmition() {
+    this.ws.send({
+      type: "authentification",
+      nickname: this.nickname,
+    });
+  }
+}
+
+export default Menu;
